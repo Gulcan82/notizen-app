21fefdbaef8d72ff2c1f31c12728b0470be5c45a
Gülcan Kamat <gulcankamat@gmail.com>
2024-04-26 13:55:57 +0200

    Adding new files to react-app, beautifyng the containers and buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,19 @@
-import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import NotesList from './components/NotesList';
+import { notes } from './data/notes.data';
 
-import NotesCard from './components/Card';
 
 export default function App() {
 
   // Die App gibt das Grundgerüst der Anwendung vor
   // Hier werden wir später viele Informationen speichern und an Komponenten weitergeben
 
-  const notes: {
-    title: string,
-    content: string,
-    categories: string[],
-    id: number
-  }[] = [
-      {
-        title: 'Überschrift 1',
-        content: 'Das ist ein langer Text',
-        categories: ['Büro', 'Haushalt'],
-        id: 1
-      }, {
-        title: 'Überschrift 2',
-        content: 'Das ist auch ein langer Text',
-        categories: ['Büro'],
-        id: 2
-      }, {
-        title: 'Überschrift 3',
-        content: 'Das ist ein besonders langer Text',
-        categories: ['Haushalt'],
-        id: 3
-      }, {
-        title: 'Überschrift 4',
-        content: 'Kurzer Text',
-        categories: [],
-        id: 4
-      }
-    ]
-
   return (
     <>
-      {
-        notes.map(note => <NotesCard
-          key={note.id}
-          title={note.title}
-          categories={note.categories}
-          content={note.content} />)
-      }
+    <Header />
+    <NotesList notes = {notes}/>
+
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,19 @@
 import Card from "react-bootstrap/Card";
+import { Note } from '../types/notes.type'
+import { Button } from "react-bootstrap";
 
-type Props = {
-  title: string,
-  content: string,
-  categories: string[]
+type Props = Note & {
+
+
 }
+  
 
 export default function NotesCard(props: Props) {
 
   const categories = props.categories.map(c => '#' + c).join(' ')
 
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card className='mb-1'>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>
@@ -20,6 +22,8 @@ export default function NotesCard(props: Props) {
         <Card.Subtitle className="mb-2 text-muted">{
           categories
         }</Card.Subtitle>
+        <Button variant="outline-success">Bearbeiten</Button>{' '}
+        <Button variant="outline-danger">löschen</Button>{' '}
       </Card.Body>
     </Card>
   )
