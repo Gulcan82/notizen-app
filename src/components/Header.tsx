606c58ce16baf418c631353aa3dc4df5f16e959f
Gülcan Kamat <gulcankamat@gmail.com>
2024-04-30 12:01:18 +0200

    Adding search functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
-import { Button, Container, Form, Navbar } from "react-bootstrap";
+import { Button, Container, Form, Navbar } from 'react-bootstrap';
 
+type Props = {
+  setSearch: React.Dispatch<React.SetStateAction<string>>
+}
 
-function Header() {
+function Header(props: Props) {
   return (
     <Navbar expand="md" className="bg-body-tertiary mb-2">
       <Container>
@@ -9,9 +12,10 @@ function Header() {
         <Form className="d-flex">
                   <Form.Control
                     type="search"
-                    placeholder="Notiz Suchen"
+                    placeholder="Notiz suchen"
                     className="me-2"
                     aria-label="Search"
+                    onChange={e => props.setSearch(e.target.value)}
                   />
                   <Button variant="outline-success">Suchen</Button>
                 </Form>
